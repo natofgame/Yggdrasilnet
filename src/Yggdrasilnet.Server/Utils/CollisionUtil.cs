6d8717bd22efc2b707ac3726d9b4177c83b7f1f0
Natof <rallonathanael@gmail.com>
2026-09-16 11:56:12 -0400

    Implement collision system

---
+++ src/Yggdrasilnet.Server/Utils/CollisionUtil.cs
@@ -0,0 +1,20 @@
+﻿using Yggdrasilnet.Shared.Compatibility;
+
+namespace Yggdrasilnet.Server.Utils;
+
+public static class CollisionUtil {
+    public static CollisionLayer GetString(string layerName) {
+        var result = CollisionLayer.None;
+        foreach (var part in layerName.Split('|')) {
+            result |= part.Trim().ToLower() switch {
+                "world"   => CollisionLayer.World,
+                "player"  => CollisionLayer.Player,
+                "monster" => CollisionLayer.Monster,
+                "spell"   => CollisionLayer.Spell,
+                "all"     => CollisionLayer.All,
+                _         => CollisionLayer.None
+            };
+        }
+        return result;
+    }
+}
---
+++ src/Yggdrasilnet.Shared/Maths/BoundingBoxes.cs
@@ -0,0 +1,58 @@
+﻿using System.Numerics;
+
+namespace Yggdrasilnet.Shared.Maths
+{
+    public struct BoundingBoxes
+    {
+        public Vector3 Min;
+        public Vector3 Max;
+
+        public BoundingBoxes(Vector3 min, Vector3 max) {
+            Min = min;
+            Max = max;
+        }
+
+        public Vector3 Center => new(
+            (Min.X + Max.X) / 2,
+            (Min.Y + Max.Y) / 2,
+            (Min.Z + Max.Z) / 2
+        );
+        
+        public Vector3 Extents => new (
+            (Max.X - Min.X) / 2,
+            (Max.Y - Min.Y) / 2,
+            (Max.Z - Min.Z) / 2
+        );
+
+        public bool Intersects(in BoundingBoxes other) {
+            return Min.X <= other.Max.X && Max.X >= other.Min.X
+                                        && Min.Y <= other.Max.Y && Max.Y >= other.Min.Y
+                                        && Min.Z <= other.Max.Z && Max.Z >= other.Min.Z;
+        }
+
+        public BoundingBoxes ExpandAlong(Vector3 direction, float distance) {
+            var offsetX = direction.X * distance;
+            var offsetY = direction.Y * distance;
+            var offsetZ = direction.Z * distance;
+            return new BoundingBoxes(
+                new Vector3(
+                    Min.X + MathF.Min(0f, offsetX),
+                    Min.Y + MathF.Min(0f, offsetY),
+                    Min.Z + MathF.Min(0f, offsetZ)),
+                new Vector3(
+                    Max.X + MathF.Max(0f, offsetX),
+                    Max.Y + MathF.Max(0f, offsetY),
+                    Max.Z + MathF.Max(0f, offsetZ))
+            );
+        }
+        
+        public static BoundingBoxes From(Vector3 center, Vector3 size)
+        {
+            var half = new Vector3(size.X * 0.5f, size.Y * 0.5f, size.Z * 0.5f);
+            return new BoundingBoxes(
+                new Vector3(center.X - half.X, center.Y - half.Y, center.Z - half.Z),
+                new Vector3(center.X + half.X, center.Y + half.Y, center.Z + half.Z)
+            );
+        }
+    }
+}
